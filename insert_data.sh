5d91d6f1ff6028ff45ee6f11121320945aeed45f
DENIS KIPRONO <61503042+Kiprengan@users.noreply.github.com>
2024-07-16 19:22:47 +0300

    World Cup Project in Free Code Camp

--- insert_data.sh
+++ insert_data.sh
@@ -1,33 +1,38 @@
-#!/bin/bash
+#! /bin/bash
 
-# Script to insert data from courses.csv and students.csv into students database
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
 
-PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
+# Do not change code above this line. Use the PSQL variable above to query your database.
+echo $"$($PSQL "truncate table games, teams")"
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WG OG
+do 
+# Check in the table first if winner exists
 
-cat courses_test.csv | while IFS="," read MAJOR COURSE
-do
-  # get major_id
-  MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
-
-  # if not found
-  if [[ -z $MAJOR_ID ]]
-  then
-    # insert major
-    INSERT_MAJOR_RESULT=$($PSQL "INSERT INTO majors(major) VALUES('$MAJOR')")
-    echo $INSERT_MAJOR_RESULT
-
-    # get new major_id
-
-  fi
-
-  # get course_id
-
-  # if not found
-
-  # insert course
-
-  # get new course_id
-
-  # insert into majors_courses
-
+if [[ $YEAR != 'year' ]]
+then
+# Select winner and loses in teams table
+WINNER_IN_TABLE=$($PSQL "select name from teams where name='$WINNER'")
+WINNER_ID=$($PSQL "select team_id from teams where name='$WINNER'")
+OPPONENT_IN_TABLE=$($PSQL "select name from teams where name='$OPPONENT'")
+OPPONENT_ID=$($PSQL "select team_id from teams where name='$OPPONENT'")
+# If not in table insert them and get their ids
+if [[ -z $WINNER_IN_TABLE ]]
+then
+#Insert Winner and get winner id
+WINNER_INTO_TABLE=$($PSQL "insert into teams(name) values('$WINNER')")
+WINNER_ID=$($PSQL "select team_id from teams where name='$WINNER'")
+fi
+if [[ -z $OPPONENT_IN_TABLE ]]
+then
+#insert opponent and get id
+OPPONENT_INTO_TABLE=$($PSQL "insert into teams(name) values('$OPPONENT')")
+OPPONENT_ID=$($PSQL "select team_id from teams where name='$OPPONENT'")
+fi
+GAME_DATA_INSERTED=$($PSQL "insert into games(year, round, winner_id, opponent_id,winner_goals,opponent_goals) values($YEAR , '$ROUND',$WINNER_ID,$OPPONENT_ID,$WG,$OG)")
+fi
 done
