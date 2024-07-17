7ce30d9e34882ce946e415fb3e758843b745c46d
DENIS KIPRONO <61503042+Kiprengan@users.noreply.github.com>
2024-07-17 23:16:25 +0300

    Bikes work

---
+++ bike-shop.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo -e "\n~~~~~ Bike Rental Shop ~~~~~\n"
+
+MAIN_MENU() {
+  echo "How may I help you?" 
+  echo -e "\n1. Rent a bike"
+  echo "2. Return a bike"
+  echo "3. Exit"
+  read MAIN_MENU_SELECTION
+}
+RENT_MENU() {
+  echo "Rent Menu"
+}
+RETURN_MENU() {
+  echo "Return Menu"
+}
+EXIT() {
+  echo -e "\nThank you for stopping in.\n"
+}
+MAIN_MENU
